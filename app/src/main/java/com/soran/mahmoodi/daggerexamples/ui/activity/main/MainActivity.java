85ce6cce1d1891705a90715a77202f5915d9fd60
Soran Mah <soranmahmoodi.com@gmail.com>
2020-09-02 23:37:19 -0700

    init commit

--- app/src/main/java/com/soran/mahmoodi/daggerexamples/ui/activity/main/MainActivity.java
+++ app/src/main/java/com/soran/mahmoodi/daggerexamples/ui/activity/main/MainActivity.java
@@ -1,29 +1,51 @@
 package com.soran.mahmoodi.daggerexamples.ui.activity.main;
 
 import androidx.annotation.NonNull;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.NavOptions;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 
+import com.google.android.material.navigation.NavigationView;
 import com.soran.mahmoodi.daggerexamples.R;
 import com.soran.mahmoodi.daggerexamples.ui.base.BaseActivity;
 import com.soran.mahmoodi.daggerexamples.ui.fargment.post.PostsFragment;
 import com.soran.mahmoodi.daggerexamples.ui.fargment.profile.ProfileFragment;
 
-public class MainActivity extends BaseActivity {
+public class MainActivity extends BaseActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        testFragment();
+        setupViews();
+        init();
     }
 
-    private void testFragment(){
-        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new PostsFragment()).commit();
+    private void setupViews() {
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
     }
+
+    private void init() {
+        NavController navController = Navigation.findNavController(this, R.id.fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout);
+        NavigationUI.setupWithNavController(navigationView, navController);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater menuInflater = getMenuInflater();
@@ -33,11 +55,47 @@ public class MainActivity extends BaseActivity {
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-      switch (item.getItemId()){
-          case R.id.item_logout:
-              sessionManger.logout();
-              break;
-      }
+        switch (item.getItemId()) {
+            case R.id.item_logout:
+                sessionManger.logout();
+                break;
+            case android.R.id.home:
+                if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+                    drawerLayout.closeDrawer(GravityCompat.START);
+                    return true;
+                } else {
+                    return false;
+                }
+        }
         return super.onOptionsItemSelected(item);
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            case R.id.item_profile:
+                NavOptions navOptions = new NavOptions.Builder().setPopUpTo(R.id.nav_main, true)
+                        .build();
+                Navigation.findNavController(this, R.id.fragment).navigate(R.id.profileFragment, null, navOptions);
+                break;
+            case R.id.item_posts:
+                if (isValidDestination(R.id.postsFragment)) {
+                    Navigation.findNavController(this, R.id.fragment).navigate(R.id.postsFragment);
+                }
+                break;
+        }
+        menuItem.setChecked(true);
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    private boolean isValidDestination(int destination) {
+        return destination != Navigation.findNavController(this, R.id.fragment).getCurrentDestination().getId();
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        return NavigationUI.navigateUp(Navigation.findNavController(this, R.id.fragment), drawerLayout);
     }
 }
